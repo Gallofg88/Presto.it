4f42a9088fd34399d1133809ae5a368f11354142
Antonio Gallo <gallofg88@gmail.com>
2024-04-19 17:39:11 +0200

    navbar da sistemare

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,104 +1,98 @@
 <!-- Contenitore principale della navbar -->
-<div class="containerNavbar container">
+<div class="container-fluid ">
     <!-- Navbar Bootstrap -->
-    <nav class="navbar navbar-expand-lg bg-navbar mynavbar fixed-top">
-        <!-- Contenitore fluido per la navbar -->
-        <div class="container">
-            <!-- Logo del sito nella navbar -->
-            <a class="navbar-brand" href="#">
-                <img id="logo" class="imagelogo" src="/img/Senza_titolo_5.png" alt="logo del sito">
-            </a>
-            <!-- Bottone per il toggle della navbar su dispositivi mobili -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!-- Contenitore per i link della navbar -->
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <!-- Lista dei link principali della navbar -->
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                    <!-- Link per la homepage -->
-                    <a class="nav-item mt-1" href="{{ route('home') }}">
-                        <i class="fa-solid fa-house"  >Home</i>
-                        
+    <nav class="navbar navbar-expand-lg bg-navbar bg-dark fixed-top">
+        <!-- Logo del sito nella navbar -->
+        <a class="navbar-brand" href="#">
+            <img id="logo" class="imagelogo" src="/img/Senza_titolo_5.png" alt="logo del sito">
+        </a>
+        <!-- Bottone per il toggle della navbar su dispositivi mobili -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Contenitore per i link della navbar -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <!-- Lista dei link principali della navbar -->
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <!-- Link per la homepage -->
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">
+                        <i class="fa-solid fa-house"></i> Home
                     </a>
-                    <!-- Link per gli annunci -->
-                    <a class="nav-item mt-3" href="{{ route('announcements.index') }}">                        
-                        <i class="fa-solid fa-bullhorn " style="color: #f7f7f7;" >Annunci</i>
-                    </a> 
-                    <!-- Link per diventare revisore -->
-                    <a class="nav-item mt-5" href="{{ route('become.revisor') }}">
-                        <i class="fa-solid fa-list-check " >Diventa revisore</i>
-                        <a class="" ></a>                
+                </li>
+                <!-- Link per gli annunci -->
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('announcements.index') }}">
+                        <i class="fa-solid fa-bullhorn"style="color: #f7f7f7;"  ></i> Annunci
                     </a>
-                    @auth
-                        <!-- Link per creare un annuncio -->              
-                        <li class="nav-item" href="{{ route('create') }}">
-                            <a class="nav-link" >Crea annunci</a>
-                        </li>
-                    @endauth
-                    <!-- Dropdown per le categorie -->
-                    <li class="nav-item dropdown mt-5">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-list">Categorie</i></a>
-                        <!-- Menu a tendina con le categorie -->
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <!-- Loop attraverso le categorie -->
-                            @foreach ($categories as $category)
-                                <li><a class="dropdown-item"
-                                        href="{{ route('categoryShow', compact('category')) }}">{{ $category->name }}</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </li>
-                </ul>
-                
-                
+                </li>
+                <!-- Link per diventare revisore -->
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('become.revisor') }}">
+                        <i class="fa-solid fa-list-check"></i> Diventa revisore
+                    </a>
+                </li>
                 @auth
-                    <div class="text-center">
-                        <!-- Messaggio di benvenuto per l'utente autenticato -->
-                        <a class="nav-link text-center">Benvenuto </a>
-                        <!-- Link per accedere al proprio profilo -->
-                        <a class="btn btn-outline-primary" type="button" href="{{ route('profile.profile') }}">{{ Auth::user()->name }}</a>
-                        <!-- Se l'utente è un revisore, mostra il link alla sezione revisore -->
-                        @if (Auth::user()->is_revisor)
-                            <div class="nav-item">
-                                <a class="badge-" href="{{ route('revisor.index') }}">Sei Revisore
-                                    <span class="badge bg-dark">
-                                        {{ App\Models\Announcement::toBeRevisionedCount() }}
-                                        <span class="visually-hidden">Articoli non revisionati</span>
-                                    </span>
-                                </a>
-                            </div>
-                        @endif
-                    </div>
+                    <!-- Link per creare un annuncio -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('create') }}">Crea annunci</a>
+                    </li>
+                @endauth
+                <!-- Dropdown per le categorie -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-list"></i> Categorie</a>
+                    <!-- Menu a tendina con le categorie -->
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <!-- Loop attraverso le categorie -->
+                        @foreach ($categories as $category)
+                            <li><a class="dropdown-item"
+                                    href="{{ route('categoryShow', compact('category')) }}">{{ $category->name }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+            
+            @auth
+                <div>
+                    <!-- Messaggio di benvenuto per l'utente autenticato -->
+                    <a class="navbar-text me-3">Benvenuto {{ Auth::user()->name }}</a>
+                    <!-- Link per accedere al proprio profilo -->
+                    <a href="{{ route('profile.profile') }}" class="btn btn-outline-primary me-3">{{ Auth::user()->name }}</a>
+                    <!-- Se l'utente è un revisore, mostra il link alla sezione revisore -->
+                    @if (Auth::user()->is_revisor)
+                        <a href="{{ route('revisor.index') }}" class="badge bg-dark me-3">Sei Revisore
+                            <span>{{ App\Models\Announcement::toBeRevisionedCount() }}</span>
+                        </a>
+                    @endif
                     <!-- Form per il logout -->
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <button type="submit" class="mx-3 button display-2 btn btn-outline-primary"><i
-                                class="fa-solid fa-right-to-bracket fa-sm" style="color: #f90404;"></i></button>
+                        <button type="submit" class="btn btn-outline-primary"><i class="fa-solid fa-right-to-bracket fa-sm" style="color: #f90404;"></i></button>
                     </form>
-                @endauth
-                <!-- Sezione per gli utenti non autenticati -->
-                @guest
-                    <div class="buttonDad my-2 my-lg-0">
-                        <!-- Bottone per registrarsi -->
-                        <a href="{{ route('register') }}" class="glow-on-hover btn btn-outline-primary"
-                            type="button">Registrati</a>
-                        <!-- Bottone per accedere -->
-                        <a href="{{ route('login') }}" class="glow-on-hover mt-1 btn btn-outline-primary"
-                            type="button">Accedi</a>
-                    </div>
-                    <div>
-                        <!-- Inclusione del componente language per l'italiano -->
-                        <x-language  lang="it" />
-                        <!-- Inclusione del componente language per l'inglese -->
-                        <x-language  lang="en" />
-                        <!-- Inclusione del componente language per lo spagnolo -->
-                        <x-language  lang="es" />
-                    </div>
-                @endguest
-            </div>
+                </div>
+            @endauth
+            
+            <!-- Sezione per gli utenti non autenticati -->
+            @guest
+                <div class="d-flex">
+                    <!-- Bottone per registrarsi -->
+                    <a href="{{ route('register') }}" class="btn btn-outline-primary me-3">Registrati</a>
+                    <!-- Bottone per accedere -->
+                    <a href="{{ route('login') }}" class="btn btn-outline-primary">Accedi</a>
+                </div>
+                <div class="d-flex mt-2">
+                    <!-- Inclusione del componente language per l'italiano -->
+                    <x-language lang="it" />
+                    <!-- Inclusione del componente language per l'inglese -->
+                    <x-language lang="en" />
+                    <!-- Inclusione del componente language per lo spagnolo -->
+                    <x-language lang="es" />
+                </div>
+            @endguest
+            
         </div>
     </nav>
 </div>
